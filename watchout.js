ed349fe454c937723ac6541bb4bb4182429306dd
danielleknudson <knudson.danielle@gmail.com>
2015-05-22 15:19:33 -0700

    make player draggable

--- watchout.js
+++ watchout.js
@@ -4,11 +4,19 @@ var svg = d3.select("body").append("svg")
   .attr("width", 700)
   .attr("height", 450)
 
+
 var player = d3.select("svg").append("path")
   // add the teardrop shape from source
   .attr("d", "m-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z")
   .attr("fill", "#ff00ff")
   .attr("transform", "translate(350,225)");
+  // .call(drag);
+var dragMove = d3.behavior.drag()
+  .on('drag', function(){
+    player.attr('transform', 'translate('+d3.event.x+','+d3.event.y+')')
+  });
+
+player.call(dragMove);
 
 // player.attr("fill", "#00ff00");
 
@@ -32,11 +40,9 @@ var enemies = svg.selectAll("circle")
 .attr("transform", newLocation);
 
 var enemyMove = function(){
-//   debugger;
   enemies
     .transition(1000)
     .attr("transform", newLocation);
 };
 
 setInterval(enemyMove, 2000);
-//  enemyMove();
